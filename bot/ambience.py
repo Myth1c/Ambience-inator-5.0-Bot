aa0ab60d8378a90819c27cb7cc04c2cfd68c860e
Mythic <Alemsm1@gmail.com>
2025-11-05 18:55:27 -0500

    Updated save functions to account for new messaging structure

--- bot/ambience.py
+++ bot/ambience.py
@@ -1,6 +1,6 @@
 # bot/ambience.py
 
-import os
+import os, json
 
 from config.json_helper import load_json, save_json
 
@@ -14,8 +14,21 @@ async def send_ambience():
 
 async def save_ambience(data):
     ambience = data.get("data")
-    if not isinstance(ambience, dict):
-        return {"command" : "ERROR", "message": "Invalid ambience data"}
     
+    # --- Parse ambience data back into a dict ---
+    if isinstance(ambience, str):
+        try:
+            playlist_data = json.loads(playlist_data)
+            print(f"[BOT] Parsed playlist data from string for 'Ambience'")
+        except Exception as e:
+            print(f"[BOT] Failed to parse playlist JSON string for 'Ambience': {e}")
+        return 
+        
+    # --- Validate ambience data ---
+    if not isinstance(ambience, dict):
+        print("[BOT] Invalid ambience save request")
+        return 
+    # --- Save ambience data ---
     save_json(AMBIENCE_FILE, ambience)
-    return len(ambience)
+    print(f"[BOT] Ambience saved with {len(ambience)} entries")
+
--- bot/playlists.py
+++ bot/playlists.py
@@ -1,6 +1,6 @@
 # bot/playlists.py
 
-import os
+import os, json
 
 from config.json_helper import load_json, save_json
 
@@ -17,11 +17,22 @@ async def save_playlist(data):
     name = data.get("name")
     playlist_data = data.get("data")
     
+    # --- Parse playlist_data back into a dict ---
+    if isinstance(playlist_data, str):
+        try:
+            playlist_data = json.loads(playlist_data)
+            print(f"[BOT] Parsed playlist data from string for {name}")
+        except Exception as e:
+            print(f"[BOT] Failed to parse playlist JSON string for '{name}': {e}")
+            return 
+    
+    # -- Validate playlist data --
     if not name or not isinstance(playlist_data, dict):
-        print("[WS] Invalid playlist save request")
-        return {"command" : "ERROR", "message": "Invalid playlist data"}
+        print("[BOT] Invalid playlist save request")
+        return
+    
+    # -- Load existing playlists for saving
     playlists = load_json(PLAYLIST_FILE, default_data={})
     playlists[name] = playlist_data
     
     save_json(PLAYLIST_FILE, playlists)
-    return len(playlists)
